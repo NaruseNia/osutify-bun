6eddd37692a2b643c5e92527ef2088d668ce9e08
NaruseNia <notshinsin0000@gmail.com>
2026-03-21 08:49:35 +0900

    chore: clean project

---
+++ src/mainview/store/useOsutifyStore.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+type RepeatMode = "none" | "all_tracks" | "one_track" | "one_track_stop"
+
+interface OsutifyStore {
+  isPlaying: boolean;
+  isShuffle: boolean;
+  repeatMode: RepeatMode;
+}
+
+export const useOsutifyStore = create<OsutifyStore>((set) => ({
+  isPlaying: false,
+  isShuffle: false,
+  repeatMode: "none",
+  togglePlaying: () => set((state) => ({ isPlaying: !state.isPlaying })),
+  toggleShuffle: () => set((state) => ({ isShuffle: !state.isShuffle })),
+  setRepeatMode: (mode: RepeatMode) => set((_) => ({ repeatMode: mode }),
+}));
